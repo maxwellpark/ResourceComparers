cae3c7ff98c4574118c2730957f5e7a2167b2e2c
Max <61809127+maxwellpark@users.noreply.github.com>
2022-08-01 21:51:10 +0100

    UpdateResourceResult fields comparison

--- ResourceComparers/Program.cs
+++ ResourceComparers/Program.cs
@@ -6,35 +6,57 @@ var requestFields = new Dictionary<string, object>
     { "foo", "f" }, { "bar", "b" }, { "baz", "b" }
 };
 
+// Original state of resource  
+var backupFields = new Dictionary<string, object>
+{
+    { "id", 1 }, { "foo", "g" }, { "bar", "c" }, { "baz", "a" }, { "quux", "q" }, { "corge", null }
+};
+
 // Response fields
 var responseFields = new Dictionary<string, object>
 {
-    { "id", 1 }, { "foo", "f" }, { "bar", "b" }, { "baz", "b" }, { "quux", null }, { "corge", null }
+    { "id", 1 }, { "foo", "f" }, { "bar", "b" }, { "baz", "b" }, { "quux", "q" }, { "corge", null }
 };
 
-var requestResult = new ResourceResult
-{
-    Fields = requestFields
-};
-
-var responseResult = new ResourceResult
-{
-    Fields = responseFields
-};
+var requestResult = new ResourceResult(requestFields);
+var responseResult = new ResourceResult(responseFields);
 var comparer = new ResourceResultComparer(new string[] { "id" });
 
-// Remove nulls (for /create)
-requestResult.Fields = requestResult.Fields
-    .Where(f => !(f.Value == null && !responseFields.ContainsKey(f.Key)))
-    .ToDictionary(x => x.Key, x => x.Value);
+//// Remove nulls (for /create)
+//requestResult.Fields = requestResult.Fields
+//    .Where(f => !(f.Value == null && !responseFields.ContainsKey(f.Key)))
+//    .ToDictionary(x => x.Key, x => x.Value);
 
-var createReqResEquals = comparer.Equals(requestResult, responseResult);
-Console.WriteLine(createReqResEquals);
+var fieldsToCompare = new Dictionary<string, object>();
+
+foreach (var field in backupFields)
+{
+    var fieldToAdd = !requestFields.ContainsKey(field.Key)
+        ? field
+        : new KeyValuePair<string, object>(field.Key, requestFields[field.Key]);
+
+    fieldsToCompare[fieldToAdd.Key] = fieldToAdd.Value;
+}
+
+var fieldsSelect = new Dictionary<string, object>(backupFields);
+backupFields.ToList().ForEach(field =>
+{
+    if (requestFields.ContainsKey(field.Key))
+        fieldsSelect[field.Key] = field.Value;
+});
+
+var equals = comparer.Equals(new ResourceResult(fieldsToCompare), responseResult);
+Console.WriteLine(equals);
 
 Console.ReadKey();
 
 class ResourceResult
 {
+    public ResourceResult(IDictionary<string, object> fields)
+    {
+        Fields = fields;
+    }
+
     public IDictionary<string, object> Fields { get; set; }
 }
 
